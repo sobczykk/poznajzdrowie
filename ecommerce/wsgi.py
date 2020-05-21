1a9a25ebe36eaeeb0dcb87891a7c770f91ff1bac
Kamila Sobczyk <kamsob11@gmail.com>
2020-05-21 10:59:23 +0200

    edit comments

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -1,10 +1,5 @@
 """
 WSGI config for ecommerce project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
