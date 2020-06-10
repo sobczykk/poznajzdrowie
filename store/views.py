e00d12b7132ea905020df6fcc7816492986d44c4
Kamila Sobczyk <kamsob11@gmail.com>
2020-06-10 13:12:50 +0200

    Paypal correction

--- store/views.py
+++ store/views.py
@@ -75,7 +75,7 @@ def processOrder(request):
 	total = float(data['form']['total'])
 	order.transaction_id = transaction_id
 
-	if total == order.get_cart_total:
+	if total == float(order.get_cart_total):
 		order.complete = True
 	order.save()
 
@@ -89,4 +89,4 @@ def processOrder(request):
 		zipcode=data['shipping']['zipcode'],
 		)
 
-	return JsonResponse('Payment submitted..', safe=False)
+	return JsonResponse('Payment complete!', safe=False)
